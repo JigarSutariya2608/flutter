ebde7d347f205ad8f1989984bfbd80cbef0def4e
jigarsutariya26 <jigarsutariya2608@gmail.com>
2021-11-08 16:43:42 +0530

    up to day 4

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/pages/home_page.dart';
-import 'package:flutter_application_1/pages/login_page.dart';
+
+import 'home_page.dart';
+import 'login_page.dart';
 
 void main() {
   runApp(const MyApp());
