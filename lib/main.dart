47eda61cc40d050a01adbc85ebf989c8a2dcc73c
jigarsutariya26 <jigarsutariya2608@gmail.com>
2021-11-08 16:55:32 +0530

    up to day 4

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      // initialRoute: "/home",
+      initialRoute: "/home",
       routes: {
         "/": (context) => const LoginPage(),
         "/login": (context) => const LoginPage(),
